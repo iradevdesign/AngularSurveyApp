650601f7456235bca67d6ddfac1e14b3c02eb269
unknown <iradev.design@gmail.com>
2020-04-01 16:55:37 +0300

    fix prev and next buttons

--- src/app/survey/survey-view/survey-view.component.ts
+++ src/app/survey/survey-view/survey-view.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, Params } from '@angular/router';
 import { SurveyService } from '../survey.service';
 import { Survey } from '../survey.model';
+import { SurveyList } from '../survey-list.model';
 
 @Component({
   selector: 'app-survey-view',
@@ -11,16 +12,32 @@ import { Survey } from '../survey.model';
 export class SurveyViewComponent implements OnInit {
   id: number;
   survey: Survey;
+  previewId: number;
+  nextId: number;
+  surveyList: SurveyList;
+  surveyListLenght: number;
 
   constructor(private route: ActivatedRoute, private router: Router, private surveyService: SurveyService) { }
 
   ngOnInit() {
     this.route.params.subscribe(
       (params: Params) => {
-        this.id = params['id'];
+        this.id = +params['id'];
         this.survey = this.surveyService.getSurvey(this.id);
+        this.previewId = this.id - 1;
+        this.nextId = this.id + 1;
+        this.surveyList = this.surveyService.getSurveysList();
+        this.surveyListLenght = this.surveyList.surveyList.length;
       }
     )
+  }
+
+  onPreview() {
+    this.router.navigate(['../', this.previewId])
+  }
+
+  onNext() {
+    this.router.navigate(['../', this.nextId])
   }
 
 }
